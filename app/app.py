7907afe29e1a6175f77770908d5896c358249d00
X Hilmi <38133643+xhilmi@users.noreply.github.com>
2024-05-08 18:29:44 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def hello_world():
     return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR'],'methods':'GET', 'ip3':str(request.headers)}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
